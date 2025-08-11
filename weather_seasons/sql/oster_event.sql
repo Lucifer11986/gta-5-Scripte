3de3bddccf1ed103d9e4fb7f8e05d89519401aa2
Lucifer11986 <dmenter21@googlemail.com>
2025-08-11 17:59:30 +0200

    Delete weather_seasons directory

--- weather_seasons/sql/oster_event.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `easter_eggs` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `egg_id` INT NOT NULL,
-    `player_id` VARCHAR(255) NOT NULL,
-    `finder_identifier` VARCHAR(255) NULL DEFAULT NULL,
-    `found_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
